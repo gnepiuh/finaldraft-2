12c4d4b889fecb9eb67fc2afbba3050e7815a742
gnepiuh <145451868+gnepiuh@users.noreply.github.com>
2024-01-10 10:30:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let Dice1 = 0;
+let Dice2 = 0;
+let Dice3 = 0;
+<img id = "DiceOne" src = "images/dice1.png"></img>
+
+
+
+function roll1Dice() {
+    Dice1 = 1 + Math.floor(Math.random() * 6);
+    if (Dice1 = 1) {
+
+    }
+
+  
+  }
